8dfe8b55d7ff26190db14c84a08d217e21d6ea07
AshryA <ahmed.ashry@vodafone.com>
2021-12-07 21:09:31 +0200

    fix: fix unit test for news feed screen

--- __tests__/Screens/NewsFeed.test.tsx
+++ __tests__/Screens/NewsFeed.test.tsx
@@ -4,7 +4,21 @@ import { render, act, fireEvent } from '@testing-library/react-native'
 
 import { NewsFeed } from '../../src/Screens'
 import { testIds } from '../../src/Screens/NewsFeed/NewsFeed.testIds'
-import { article } from '../stubs'
+
+const article = {
+  source: {
+    id: '',
+    name: ''
+  },
+  author: 'author',
+  title: 'title1',
+  description: 'description',
+  url: '',
+  urlToImage:
+    'https://s.yimg.com/os/creatr-uploaded-images/2021-12/6f5ec8f0-55e4-11ec-ad4d-0378eb0e51e1',
+  publishedAt: '2021-12-13',
+  content: ''
+}
 
 jest.mock('../../src/Services/Apis', () => {
   return {
