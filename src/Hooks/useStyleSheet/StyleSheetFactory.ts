6c9b0612d2f920383bee45cb547065317b104b81
AshryA <ahmed.ashry@vodafone.com>
2021-12-08 21:16:35 +0200

    style: enhance StyleSheetFactory use momoize from lodash to memoize the themed style sheet

--- src/Hooks/useStyleSheet/StyleSheetFactory.ts
+++ src/Hooks/useStyleSheet/StyleSheetFactory.ts
@@ -1,10 +1,11 @@
-// import { memoize } from 'lodash'
+// @ts-ignore
+import { memoize } from 'lodash'
 
 import { ThemeType } from '../../Themes/types'
 
 const StyleSheetFactory = (theme: ThemeType, styles: any) => {
-  // const memoizedStyles = memoize(styles)
-  return styles(theme)
+  const memoizedStyles = memoize(styles)
+  return memoizedStyles(theme)
 }
 
 export default StyleSheetFactory
