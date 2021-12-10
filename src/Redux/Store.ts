ce0d4d040904e451114b5e44523be9cc5e842c35
AshryA <ahmed.ashry@vodafone.com>
2021-12-10 19:16:41 +0200

    feat: presist app language use asyncstorage for presist App language with redux-presist

--- src/Redux/Store.ts
+++ src/Redux/Store.ts
@@ -1,13 +1,37 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import { persistReducer, persistStore } from 'redux-persist'
+import AsyncStorage from '@react-native-community/async-storage'
 
 import { settingsReducer } from './Reducers/setting.reducer'
 
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage: AsyncStorage,
+  whitelist: ['settings'],
+  blacklist: []
+}
+
+const settingPersistConfig = {
+  key: 'settings',
+  version: 1,
+  storage: AsyncStorage,
+  whitelist: ['language']
+}
+
 const reducers = combineReducers({
-  settings: settingsReducer
+  settings: persistReducer(settingPersistConfig, settingsReducer)
 })
+
+const persistedReducer = persistReducer(persistConfig, reducers)
 
 const store = configureStore({
-  reducer: reducers
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false
+    })
 })
-
+const persistor = persistStore(store)
+export const _persistor = persistor
 export default store
